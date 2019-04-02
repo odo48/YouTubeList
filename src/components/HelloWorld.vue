ac72859b51de328789a18baf27f007aa09fb53ee
odo48 <teodordacu@gmail.com>
2019-04-03 01:40:52 +0300

    addded functionality for play another video

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="hello">
-    <div class="search">
-      <input ref="inputVideo">
-      <button class="btn-primary btn btn-play" @click="addVideo()">Play</button>
+    <div class="search-container">
+      <input class="col-lg-3 search" placeholder="Insert Youtube URL" ref="inputVideo">
+      <button class="btn-play btn btn-danger" @click="addVideo()"><i class="fas fa-search"></i></button>
     </div>
     <div id="player"></div>
+    <div class="video-list"></div>
   </div>
 </template>
 
@@ -14,7 +15,8 @@ export default {
   data() {
     return {
       newVideo: null,
-      thisId: null
+      thisId: null,
+      videoList: []
     }
   },
   methods: {
@@ -23,6 +25,7 @@ export default {
       this.youtubeParser(this.newVideo)
     },
     youtubeParser(url) {
+      var YT = window.YT.get('player')
       var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/
       var match = url.match(regExp)
       if (match&&match[7].length === 11) {
@@ -31,7 +34,8 @@ export default {
           this.onYouTubeIframeAPIReady(this.thisId)
         } else {
           this.thisId = match[7]
-          this.onYouTubeIframeAPIReady(this.thisId)
+          this.videoList.push(this.thisId)
+          console.log(this.videoList)
         }
       } 
     },
@@ -45,11 +49,27 @@ export default {
         videoId: idUrl,
         playerVars: { 'rel': 0 },
         events: {
-          'onReady': onPlayerReady
+          'onReady': onPlayerReady,
+          'onStateChange': onPlayerStateChange
         }
       })
       function onPlayerReady(event) {
         event.target.playVideo()
+      }
+      function onPlayerStateChange(event) {
+        if (event.data === YT.PlayerState.PLAYING) {
+          player.getOptions()
+          console.log('play')
+        } else if (event.data === YT.PlayerState.ENDED) {
+          console.log('ended')
+          player.playVideo()
+        } else if (event.data === YT.PlayerState.PAUSED) {
+          console.log('paused')
+        } else if (event.data === YT.PlayerState.BUFFERING) {
+          console.log('buffering')
+        } else {
+          console.log('cued')
+        }
       }
     }
   },
@@ -62,7 +82,31 @@ export default {
 }
 </script>
 <style scoped>
-.btn-play {
-  margin-left: 20px;
+.search-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 16px;
 }
+.btn-play {
+  border-radius: 0px;
+  padding: 1px 20px;
+  outline: 0!important;
+  font-size: 20px;
+  border-radius: 0px 10px 10px 0px;
+}
+.btn-danger:focus, .btn-danger:enabled, .btn-danger:active{
+  box-shadow: none!important;
+}
+.search {    
+  padding: 1px 10px;
+  outline: none;
+  font-size: 20px;
+  border: 1px solid #00000038;
+  border-radius: 10px 0px 0px 10px;
+}
+.search::placeholder {
+  
+}
+
 </style>
